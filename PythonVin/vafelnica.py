a7eabcf30a11e3c698f06386ce7edb7e307fe376
Vinlimchik <114074087+Vinlimchik@users.noreply.github.com>
2023-03-22 16:29:50 +0300

    Add files via upload

---
+++ PythonVin/vafelnica.py
@@ -0,0 +1,15 @@
+#x = 1
+#n = input("Enter a random number: ")
+#for step in range (1,int(n)+1):
+    #x = x * step
+    #print(x)
+
+num = input("Enter a number: ")
+while num != ("exit"):
+    num = input("Enter a number: ")
+if %(num) == 0:
+    num = x
+
+
+    
+    
