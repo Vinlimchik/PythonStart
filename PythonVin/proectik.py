d6e12b48a6c14e63ce3fdfa0a21420d89b667558
Vinlimchik <114074087+Vinlimchik@users.noreply.github.com>
2023-04-26 16:11:50 +0300

    Update proectik.py

--- PythonVin/proectik.py
+++ PythonVin/proectik.py
@@ -1,68 +1,103 @@
-nom = input("select operation: +, -, *, /, //, !, √, exit: ")
+nom = input("select operation: +, -, *, /, //, !, √^2, √^3, ^n, multitask, exit: ")
 num = 1
 nim = 1
 nym = 1
-while (nom == "exit"):
-    print("it's all folks")
+ibm = 1
 
 while nom != "exit":
 
+#+
     if nom == "+":
         num = input("Enter a first number: ")
         nim = input("Enter a second number: ")
         x = int(num) + int(nim)
         int(num) + int(nim)
         print(x)
-        nom = input("select operation: +, -, *, /, //, !, √, exit: ")
-    
+        nom = input("select operation: +, -, *, /, //, !, √^2, √^3, ^n, multitask, exit: ")
+
+#-
     elif nom =="-":
         num = input("Enter a first number: ")
         nim = input("Enter a second number: ")
         y = int(num) - int(nim)
         int(num) - int(nim)
         print(y)
-        nom = input("select operation: +, -, *, /, //, !, √, exit: ")
+        nom = input("select operation: +, -, *, /, //, !, √^2, √^3, ^n, multitask, exit: ")
         
+#*
     elif nom == "*":
         num = input("Enter a first number: ")
         nim = input("Enter a second number: ")
         u = int(num) * int(nim)
         int(num) * int(nim)
         print(u)
-        nom = input("select operation: +, -, *, /, //, !, √, exit: ")
-        
+        nom = input("select operation: +, -, *, /, //, !, √^2, √^3, ^n, multitask, exit: ")
+
+#/
     elif nom == "/":
         num = input("Enter a first number: ")
         nim = input("Enter a second number: ")
         i = int(num) / int(nim)
         int(num) / int(nim)
         print(i)
-        nom = input("select operation: +, -, *, /, //, !, √, exit: ")
+        nom = input("select operation: +, -, *, /, //, !, √^2, √^3, ^n, multitask, exit: ")
 
+#//
     elif nom == "//":
         num = input("Enter a first number: ")
         nim = input("Enter a second number: ")
         o = int(num) // int(nim)
         int(num) // int(nim)
         print(o)
-        nom = input("select operation: +, -, *, /, //, !, √, exit: ")
-    
+        nom = input("select operation: +, -, *, /, //, !, √^2, √^3, ^n, multitask, exit: ")
+
+#n!
     elif nom == "!":
         nym = input("Enter a number: ")
         p = 1
         for step in range (1,int(nym)+1):
             p = p * step
             print(p)
-        nom = input("select operation: +, -, *, /, //, !, √, exit: ")
+        nom = input("select operation: +, -, *, /, //, !, √^2, √^3, ^n, multitask, exit: ")
         
-    elif nom == "√":
+#√^2
+    elif nom == "√^2":
         nym = input("Enter a number: ")
-        
-        nom = input("select operation: +, -, *, /, //, !, √, exit: ")
-            
-    elif nom != "+" or "-" or "*" or "/" or "//" or "!" or "√" or "exit":
+        sqrt = int(nym) ** (0.5)
+        print(sqrt)
+        nom = input("select operation: +, -, *, /, //, !, √^2, √^3, ^n, multitask, exit: ")
+
+#√^3
+    elif nom == "√^3":
+        nym = input("Enter anumber: ")
+        cbrt = int(nym) ** (1./3.)
+        print(cbrt)
+        nom = input("select operation: +, -, *, /, //, !, √^2, √^3, ^n, multitask, exit: ")
+    
+#^n
+    elif nom == "^n":
+        nym = input("Select degree: ")
+        ibm = input("Enter a number: ")
+        d = nym
+        t = int(ibm)
+        result = 1
+        for i in range(1,int(d)+1):
+            result = result * t
+        print(result)
+        nom = input("select operation: +, -, *, /, //, !, √^2, √^3, ^n, multitask, exit: ")
+    
+#multitask
+    elif nom == "multitask":
+        nym = input("Enter first number: ")
+        nom = input("Enter first operation: +, -, *, /, //, !, √^2, √^3, ^n: ")
+        nim = input("Enter second number: ")
+        num = input("Enter second operation: +, -, *, /, //, !, √^2, √^3, ^n: ")
+        result = int(nym) + int(num)
+    
+#other
+    elif nom != "+" or "-" or "*" or "/" or "//" or "!" or "√" or "^n" or "exit":
         print("Wrong!")
-        nom = input("select operation: +, -, *, /, //, !, √, exit: ")
+        nom = input("select operation: +, -, *, /, //, !, √^2, √^3, ^n, multitask, exit: ")
         
 if nom == "exit":
-    print("meooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooow")
+    print("meow")
