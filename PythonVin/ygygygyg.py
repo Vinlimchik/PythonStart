c4d2d9b584c108255dbad0e4973dcb3436dccd26
Vinlimchik <114074087+Vinlimchik@users.noreply.github.com>
2023-05-10 20:28:33 +0700

    Update ygygygyg.py

--- PythonVin/ygygygyg.py
+++ PythonVin/ygygygyg.py
@@ -12,7 +12,9 @@ if massive == "show":
     for i in range(0,len(list)):
         if list[i] >= 0:
             sum_positive = sum_positive + 1
-            print(sum_positive)
+            
         if list[i] < 0:
             sum_negative = sum_negative + 1
-            print(sum_negative)
+            
+    print(sum_positive)
+    print(sum_negative)
