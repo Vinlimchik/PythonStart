7e61c95b1cfd73f2a17627f02d0fae1f309231c2
Vinlimchik <114074087+Vinlimchik@users.noreply.github.com>
2023-05-17 20:09:22 +0700

    Add files via upload

---
+++ PythonVin/labirintik.py
@@ -0,0 +1,90 @@
+#Variables
+o = "0"
+x = 0
+y = 0
+num = 0
+
+#massive
+field = [["*", "*", "|", "_", "_", "_", "_", "_", "", ""],
+            ["|", "*", "*", "*", "*", "*", "*", "*", "|", ""],
+            ["|", "*", "|", "--", "--", "--", "*", "--", "", ""],
+            ["|", "*", "|", "*", "*", "|", "*", "|", "*", "*"],
+            ["|", "*", "|", "_", "_", "|", "*", "|", "*", "*"],
+            ["|", "*", "|", "*", "*", "--", "--", "*", "*", "*"],
+            ["|", "*", "|", "*", "*", "*", "*", "*", "*", "*"],
+            ["|", "*", "*", "*", "*", "*", "*", "*", "*", "*"],
+            ["*", "*", "*", "*", "*", "*", "*", "*", "*", "*"],
+            ["*", "*", "*", "*", "*", "*", "*", "*", "*", "*"]]
+
+field[x][y] = o
+
+for line in field:
+    for i in line:
+        print(i,end='')
+    print()
+num = input("Enter command: w, a, s, d or exit: ")
+
+#Cycle
+while num!= "exit":
+    
+#W
+    if num == "w":
+        if x > 0:
+            x = x - 1
+            field[x][y] = o
+            field[x+1][y] = "*"
+        else:
+            print("Wrong!")
+        field[x][y] = o
+        field[x+1][y] = "*"
+        for line in field:
+            for i in line:
+                print(i,end='')
+            print()
+        num = input("Enter command: w, a, s, d or exit: ")
+
+#A
+    if num == "a":
+        if y > 0:
+            y = y - 1
+            field[x][y] = o
+            field[x][y+1] = "*"
+        else:
+            print("Wrong!")
+        for line in field:
+            for i in line:
+                print(i,end='')
+            print()
+        num = input("Enter command: w, a, s, d or exit: ")
+
+#S  
+    if num == "s":
+        if x < 9:
+            x = x + 1
+            field[x][y] = o
+            field[x-1][y] = "*"
+        else:
+            print("Wrong!")
+        for line in field:
+            for i in line:
+                print(i,end='')
+            print()
+        num = input("Enter command: w, a, s, d or exit: ")
+
+#D
+    if num == "d":
+        if y < 9:
+            y = y + 1
+            field[x][y] = o
+            field[x][y-1] = "*"
+        else:
+            print("Wrong!")
+        for line in field:
+            for i in line:
+                print(i,end='')
+            print()
+        num = input("Enter command: w, a, s, d or exit: ")
+        
+#Exit
+if num == "exit":
+    print("Arevouar, Shoshanna!")
